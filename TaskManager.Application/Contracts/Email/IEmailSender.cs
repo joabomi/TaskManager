9a44cb5762a3dc1ca164fcac81224206c13ff2da
Joaquin <agaliarepth85@gmail.com>
2025-04-08 20:39:18 +0200

    [Added] Email Service (SendGrid)

---
+++ TaskManager.Application/Contracts/Email/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Application.Models.Email;
+
+namespace TaskManager.Application.Contracts.Email;
+
+public interface IEmailSender
+{
+    Task<bool> SendEmail(Models.Email.Email email);
+}
---
+++ TaskManager.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskManager.Application.Models.Email;
+
+namespace TaskManager.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            return services;
+        }
+    }
+}
